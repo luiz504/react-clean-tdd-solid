1af795f8269d30d66837ef4bca4a6a4e92483d43
luiz bueno <luiz504@hotmail.com>
2024-06-08 18:18:45 -0300

    test: ensure AxiosHttpClient return the correct values

--- src/infra/http/axios-http-adapter/axios-http-adapter.spec.ts
+++ src/infra/http/axios-http-adapter/axios-http-adapter.spec.ts
@@ -18,10 +18,33 @@ const mockPostRequest = (): HttpPostParams<any> => ({
 })
 
 describe('AxiosHttpAdapter', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
   it('Should call axios with correct values', () => {
+    const mockedAxiosResult = {
+      status: faker.number.int(),
+      data: faker.finance.currency(),
+    }
+    mockedAxios.post.mockResolvedValue(mockedAxiosResult)
     const { sut } = makeSut()
     const request = mockPostRequest()
     sut.post(request)
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+  })
+
+  it('Should return the correct statusCode and body', async () => {
+    const mockedAxiosResult = {
+      status: faker.number.int(),
+      data: faker.finance.currency(),
+    }
+    mockedAxios.post.mockResolvedValue(mockedAxiosResult)
+    const { sut } = makeSut()
+    const httpResponse = await sut.post(mockPostRequest())
+
+    expect(httpResponse).toEqual({
+      statusCode: mockedAxiosResult.status,
+      body: mockedAxiosResult.data,
+    })
   })
 })
