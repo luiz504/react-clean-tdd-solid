af1631551099e6d5b3c6e2474a682cb306481ba7
luiz bueno <luiz504@hotmail.com>
2024-06-08 16:06:38 -0300

    test: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/__test__/mock-http-client.ts
+++ src/data/__test__/mock-http-client.ts
@@ -2,12 +2,21 @@ import {
   HttpPostClient,
   HttpPostParams,
 } from '../protocols/http/http-post-client'
+import {
+  HttpResponse,
+  HttpStatusCode,
+} from '../protocols/http/http-post-response'
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string
   body?: object
-  async post(params: HttpPostParams): Promise<void> {
+  response: HttpResponse = {
+    statusCode: HttpStatusCode.noContent,
+  }
+
+  async post(params: HttpPostParams): Promise<HttpResponse> {
     this.url = params.url
     this.body = params.body
+    return Promise.resolve(this.response)
   }
 }
--- src/data/use-cases/authentication/remote-authentication.spec.ts
+++ src/data/use-cases/authentication/remote-authentication.spec.ts
@@ -3,6 +3,8 @@ import { HttpPostClientSpy } from '~/data/__test__/mock-http-client'
 
 import { RemoteAuthentication } from './remote-authentication'
 import { mockAuthentication } from '~/domain/__test__/mock-authentication'
+import { HttpStatusCode } from '~/data/protocols/http/http-post-response'
+import { InvalidCredentialsError } from '~/domain/errors/invalid-credentials-error'
 
 const makeSut = (url: string = faker.internet.url()) => {
   const httpPostClientSpy = new HttpPostClientSpy()
@@ -26,5 +28,13 @@ describe('RemoteAuthentication', () => {
     const credentials = mockAuthentication()
     await sut.auth(credentials)
     expect(httpPostClientSpy.body).toEqual(credentials)
+  })
+  it('should throw InvalidCredentialsError if HttpClient returns 401', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.unauthorized,
+    }
+    const promise = sut.auth(mockAuthentication())
+    await expect(() => promise).rejects.toThrow(new InvalidCredentialsError())
   })
 })
