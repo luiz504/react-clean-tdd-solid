0f0887f9f4770886c257e1ce1a6347a020503552
luiz bueno <luiz504@hotmail.com>
2024-06-08 18:56:31 -0300

    refactor: move main file to main layer

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
 
-import './styles/global.css'
+import '~/presentation/styles/global.css'
+import { App } from '~/presentation/App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
