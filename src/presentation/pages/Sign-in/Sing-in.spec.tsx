92bf2c9e64233ee2360997728a27d2895965b852
luiz bueno <luiz504@hotmail.com>
2024-06-09 15:17:21 -0300

    test: ensure sign-in calls validation with correct email value

--- src/presentation/pages/Sign-in/Sing-in.spec.tsx
+++ src/presentation/pages/Sign-in/Sing-in.spec.tsx
@@ -1,9 +1,26 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { SignIn } from '.'
+import { Validation } from '~/presentation/protocols/validation'
 
+class ValidationSpy implements Validation {
+  errorMessage?: string = undefined
+  input: object = {}
+
+  validate(input: object): string {
+    this.input = input
+    return this.errorMessage
+  }
+}
+const makeSut = () => {
+  const validationSpy = new ValidationSpy()
+  render(<SignIn validation={validationSpy} />)
+
+  return { validationSpy }
+}
 describe('Page: Sing-in', () => {
   it('should render correctly with initial state', () => {
-    render(<SignIn />)
+    makeSut()
+
     const formStatus = screen.getByTestId('form-status')
 
     expect(formStatus).toBeInTheDocument()
@@ -22,5 +39,13 @@ describe('Page: Sing-in', () => {
     const link = screen.getByTestId('sign-up-link')
     expect(link).toBeVisible()
     expect(link).toHaveAttribute('href', '/sign-up')
+  })
+
+  it('should call validation with correct input value', () => {
+    const { validationSpy } = makeSut()
+    const emailInput = screen.getByTestId('email-input')
+
+    fireEvent.input(emailInput, { target: { value: 'any_email' } })
+    expect(validationSpy.input).toEqual({ email: 'any_email' })
   })
 })
