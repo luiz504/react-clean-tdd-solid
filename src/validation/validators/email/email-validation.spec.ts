dd8ba16e12166b412ce4d36fc3810d4cb36848ee
luiz bueno <luiz504@hotmail.com>
2024-06-16 16:59:12 -0300

    test: ensure EmailValidation keep its fieldName

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -2,14 +2,23 @@ import { faker } from '@faker-js/faker'
 import { EmailValidation } from './email-validation'
 import { InvalidFieldError } from '~/validation/errors'
 
+const makeSut = (fieldName = faker.database.column()): EmailValidation => {
+  return new EmailValidation(fieldName)
+}
+
 describe('EmailValidation', () => {
+  it('should keep the field name when initialized', () => {
+    const fieldName = faker.database.column()
+    const sut = makeSut(fieldName)
+    expect(sut.fieldName).toBe(fieldName)
+  })
   it('Should return error if email is invalid', () => {
-    const sut = new EmailValidation(faker.database.column())
+    const sut = makeSut()
     const error = sut.validate(faker.word.adverb())
     expect(error).toEqual(new InvalidFieldError())
   })
   it('Should return null if email is valid', () => {
-    const sut = new EmailValidation(faker.database.column())
+    const sut = makeSut()
     const error = sut.validate(faker.internet.email())
     expect(error).toBeNull()
   })
