0cc760cf99949da8ba2861c18c229df8afca3305
luiz bueno <luiz504@hotmail.com>
2024-06-16 16:57:37 -0300

    test: ensure EmailValidation return null if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,16 @@
+import { faker } from '@faker-js/faker'
 import { EmailValidation } from './email-validation'
 import { InvalidFieldError } from '~/validation/errors'
 
 describe('EmailValidation', () => {
   it('Should return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
-    const error = sut.validate()
+    const sut = new EmailValidation(faker.database.column())
+    const error = sut.validate(faker.word.adverb())
     expect(error).toEqual(new InvalidFieldError())
+  })
+  it('Should return null if email is valid', () => {
+    const sut = new EmailValidation(faker.database.column())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeNull()
   })
 })
