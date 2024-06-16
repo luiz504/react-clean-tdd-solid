9f8902f0c4f2abbde7a7c4d208abcf761d665b0d
luiz bueno <luiz504@hotmail.com>
2024-06-16 16:38:28 -0300

    test: ensure fieldName is persisted

--- src/validation/validations/required-field/required-field-validation.spec.ts
+++ src/validation/validations/required-field/required-field-validation.spec.ts
@@ -2,11 +2,18 @@ import { RequiredFieldError } from '~/validation/errors'
 import { RequiredFieldValidation } from './required-field-validation'
 import { faker } from '@faker-js/faker'
 
-const makeSut = () => new RequiredFieldValidation(faker.database.column())
+const makeSut = (fieldName = faker.database.column()) =>
+  new RequiredFieldValidation(fieldName)
 
 describe('RequiredFieldValidation', () => {
+  it('should keep the field name when initialized', () => {
+    const fieldName = faker.database.column()
+    const sut = makeSut(fieldName)
+    expect(sut.fieldName).toBe(fieldName)
+  })
   it('Should return error if field is empty', () => {
     const sut = makeSut()
+
     const error = sut.validate('')
 
     expect(error).toEqual(new RequiredFieldError())
