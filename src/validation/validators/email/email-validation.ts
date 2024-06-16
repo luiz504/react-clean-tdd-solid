6c23258a6353c35da90e97abe858cd98d318011b
luiz bueno <luiz504@hotmail.com>
2024-06-16 16:56:55 -0300

    feat: ensure EmailValidation return null if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -1,10 +1,13 @@
 import { InvalidFieldError } from '~/validation/errors'
 import { FieldValidation } from '~/validation/protocols/field-validation'
 
+const complexEmailRegex =
+  // eslint-disable-next-line no-control-regex
+  /^(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-zA-Z0-9-]*[a-zA-Z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*)\])$/
+
 export class EmailValidation implements FieldValidation {
   constructor(readonly fieldName: string) {}
-  validate() // fieldValue: string
-  : Error | null {
-    return new InvalidFieldError()
+  validate(fieldValue: string): Error | null {
+    return complexEmailRegex.test(fieldValue) ? null : new InvalidFieldError()
   }
 }
