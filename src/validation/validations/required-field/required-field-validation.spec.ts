64d68328b5a5abb3ddf767b475c56d6cf2e099cf
luiz bueno <luiz504@hotmail.com>
2024-06-16 16:31:49 -0300

    test: ensure RequiredFieldValidation return null if field is filled

--- src/validation/validations/required-field/required-field-validation.spec.ts
+++ src/validation/validations/required-field/required-field-validation.spec.ts
@@ -1,5 +1,6 @@
 import { RequiredFieldError } from '~/validation/errors'
 import { RequiredFieldValidation } from './required-field-validation'
+import { faker } from '@faker-js/faker'
 
 describe('RequiredFieldValidation', () => {
   it('Should return error if field is empty', () => {
@@ -7,5 +8,11 @@ describe('RequiredFieldValidation', () => {
     const error = sut.validate('')
 
     expect(error).toEqual(new RequiredFieldError())
+  })
+  it('should return null if field is not empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate(faker.lorem.word())
+
+    expect(error).toBeNull()
   })
 })
