58b028123b82ee35e0668312b819f8f75013ede9
luiz bueno <luiz504@hotmail.com>
2024-06-22 20:12:08 -0300

    fix: change AxiosHttPAdapt error handling

--- src/infra/__test__/mock-axios.ts
+++ src/infra/__test__/mock-axios.ts
@@ -2,12 +2,13 @@ import axios from 'axios'
 import { Mocked } from 'vitest'
 import { faker } from '@faker-js/faker'
 
+export const mockHttpResponse = () => ({
+  status: faker.number.int(),
+  data: faker.finance.currency(),
+})
 export const mockAxios = () => {
   const mockedAxios = axios as Mocked<typeof axios>
-  mockedAxios.post.mockResolvedValue({
-    status: faker.number.int(),
-    data: faker.finance.currency(),
-  })
+  mockedAxios.post.mockResolvedValue(mockHttpResponse())
 
   return { mockedAxios }
 }
