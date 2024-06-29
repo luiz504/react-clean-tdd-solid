798632440fa64cbcddd0f441f6cd3810bf893eed
luiz bueno <luiz504@hotmail.com>
2024-06-29 12:06:38 -0300

    refactor: move sut to its own factory

--- src/data/use-cases/save-access-token/local-save-access-token.spec.ts
+++ src/data/use-cases/save-access-token/local-save-access-token.spec.ts
@@ -2,13 +2,19 @@ import { faker } from '@faker-js/faker'
 import { StorageSpy } from '~/data/__test__'
 import { LocalSaveAccessToken } from './local-save-access-token'
 
+const makeSut = () => {
+  const storageSpy = new StorageSpy()
+  const sut = new LocalSaveAccessToken(storageSpy)
+  return { sut, storageSpy }
+}
+
 describe('LocalSaveAccessToken', () => {
   it('should call Storage.save with correct values', async () => {
-    const storageSpy = new StorageSpy()
-    const sut = new LocalSaveAccessToken(storageSpy)
-    const accessToken = faker.string.uuid()
+    const { storageSpy, sut } = makeSut()
 
+    const accessToken = faker.string.uuid()
     await sut.save(accessToken)
+
     expect(storageSpy.key).toBe('accessToken')
     expect(storageSpy.value).toBe(accessToken)
   })
