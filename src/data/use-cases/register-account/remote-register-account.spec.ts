913457e1ac5c9c482e8175aba64e082ad97f6636
luiz bueno <luiz504@hotmail.com>
2024-06-30 11:39:28 -0300

    test: ensure RemoteRegisterAccount throws EmailInUseError when receive 403

--- src/data/use-cases/register-account/remote-register-account.spec.ts
+++ src/data/use-cases/register-account/remote-register-account.spec.ts
@@ -3,6 +3,7 @@ import { HttpPostClientSpy } from '~/data/__test__'
 import { RemoteRegisterAccount } from './remote-register-account'
 import { mockAccountModel, mockRegisterAccountParams } from '~/domain/__test__'
 import { HttpStatusCode } from '~/data/protocols/http'
+import { EmailInUserError } from '~/domain/errors'
 
 const makeSut = (url: string = faker.internet.url()) => {
   const httpPostClientSpy = new HttpPostClientSpy()
@@ -26,5 +27,13 @@ describe('RemoteRegisterAccount', () => {
     await sut.register(account)
     expect(httpPostClientSpy.url).toBe(url)
     expect(httpPostClientSpy.body).toEqual(account)
+  })
+  it('should throw EmailInUseError if HttpClient returns 403', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden,
+    }
+    const promise = sut.register(mockRegisterAccountParams())
+    await expect(() => promise).rejects.toThrow(new EmailInUserError())
   })
 })
