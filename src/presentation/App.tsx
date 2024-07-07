c609ea51e689554b7fca26c10d5755d28a03a4fb
luiz bueno <luiz504@hotmail.com>
2024-07-07 18:52:57 -0300

    feat: implement sign-up registerAccount factory

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -4,5 +4,5 @@ import { MakeSignIn } from '~/main/factories/pages/sign-in/sign-in-factory'
 import { MakeSignUp } from '~/main/factories/pages/sign-up/sign-up-factory'
 
 export function App() {
-  return <Router MakeSignIn={<MakeSignIn />} MakeSignUp={<MakeSignUp />} />
+  return <Router makeSignIn={<MakeSignIn />} makeSignUp={<MakeSignUp />} />
 }
--- src/presentation/router/index.tsx
+++ src/presentation/router/index.tsx
@@ -1,16 +1,16 @@
 import { FC, ReactNode } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-type Props = {
-  MakeSignIn: ReactNode
-  MakeSignUp: ReactNode
+type Factory = {
+  makeSignIn: ReactNode
+  makeSignUp: ReactNode
 }
-export const Router: FC<Props> = ({ MakeSignIn, MakeSignUp }) => {
+export const Router: FC<Factory> = (factory) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/sign-in" element={MakeSignIn} />
-        <Route path="/sign-up" element={MakeSignUp} />
+        <Route path="/sign-in" element={factory.makeSignIn} />
+        <Route path="/sign-up" element={factory.makeSignUp} />
       </Routes>
     </BrowserRouter>
   )
