71ae80f977c9701d6a0563bc095b8c7a7c5cef58
luiz bueno <luiz504@hotmail.com>
2024-07-20 16:40:42 -0300

    refactor: move cypress folder to main folder

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
-    specPattern: 'cypress/integration/**/*.{cy,spec}.{js,ts,jsx,tsx}',
+    specPattern: 'src/**/cypress/integration/**/*.{cy,spec}.{js,ts,jsx,tsx}',
+    supportFile: 'src/main/__test__/cypress/support/e2e.ts',
     fixturesFolder: false,
   },
 })
