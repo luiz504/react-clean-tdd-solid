c390dc80ff4d81d62b8bc8158f95c41022a26837
luiz bueno <luiz504@hotmail.com>
2024-07-20 17:34:18 -0300

    test: ensure form does call api if any required field empty or invalid

--- src/main/__test__/cypress/integration/sign-in.spec.ts
+++ src/main/__test__/cypress/integration/sign-in.spec.ts
@@ -1,4 +1,5 @@
-const elementId = {
+import { faker } from '@faker-js/faker'
+const elementsId = {
   emailInput: 'email-input',
   emailError: 'email-error',
   pwInput: 'pw-input',
@@ -12,26 +13,37 @@ describe('Sign in', () => {
   beforeEach(() => {
     cy.visit('/sign-in')
   })
-  it('should render sign in page correctly with initial states', () => {
-    cy.getByTestId(elementId.emailInput)
+  it('should render page correctly with initial states', () => {
+    cy.getByTestId(elementsId.emailInput)
       .should('have.attr', 'type', 'email')
       .and('be.enabled')
       .and('be.empty')
       .and('have.attr', 'placeholder', 'Email')
-    cy.getByTestId(elementId.emailError).should('not.exist')
+    cy.getByTestId(elementsId.emailError).should('not.exist')
 
-    cy.getByTestId(elementId.pwInput)
+    cy.getByTestId(elementsId.pwInput)
       .should('have.attr', 'type', 'password')
       .and('be.enabled')
       .and('be.empty')
       .and('have.attr', 'placeholder', 'Password')
-    cy.getByTestId(elementId.pwError).should('not.exist')
+    cy.getByTestId(elementsId.pwError).should('not.exist')
 
-    cy.getByTestId(elementId.submitButton)
+    cy.getByTestId(elementsId.submitButton)
       .should('be.enabled')
       .and('have.text', 'Enter')
 
-    cy.getByTestId(elementId.spinner).should('not.exist')
-    cy.getByTestId(elementId.formError).should('not.exist')
+    cy.getByTestId(elementsId.spinner).should('not.exist')
+    cy.getByTestId(elementsId.formError).should('not.exist')
+  })
+  it('should show errors if form is invalid', () => {
+    cy.getByTestId(elementsId.emailInput).type(faker.lorem.word())
+    cy.getByTestId(elementsId.emailError).should('have.text', 'Invalid field')
+
+    cy.getByTestId(elementsId.pwInput).type(
+      faker.lorem.word({ length: { min: 0, max: 4 } }),
+    )
+    cy.getByTestId(elementsId.pwError).should('have.text', 'Invalid field')
+    cy.getByTestId(elementsId.spinner).should('not.exist')
+    cy.getByTestId(elementsId.formError).should('not.exist')
   })
 })
