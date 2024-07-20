d512ae7c4f710b1802891ba762378b605b5cf531
luiz bueno <luiz504@hotmail.com>
2024-07-20 16:30:59 -0300

    test: ensure login begin with correct inital state

--- cypress/integration/sign-in.spec.ts
+++ cypress/integration/sign-in.spec.ts
@@ -1,8 +1,37 @@
-import { expect } from 'chai'
+const elementId = {
+  emailInput: 'email-input',
+  emailError: 'email-error',
+  pwInput: 'pw-input',
+  pwError: 'pw-error',
+  submitButton: 'submit-button',
+  spinner: 'form-status-spinner',
+  formError: 'form-status-error',
+} as const
 
 describe('Sign in', () => {
-  it('should render sign in page correctly with initial states', () => {
+  beforeEach(() => {
     cy.visit('/sign-in')
-    expect(1 + 1).to.equal(2)
+  })
+  it('should render sign in page correctly with initial states', () => {
+    cy.getByTestId(elementId.emailInput)
+      .should('have.attr', 'type', 'email')
+      .and('be.enabled')
+      .and('be.empty')
+      .and('have.attr', 'placeholder', 'Email')
+    cy.getByTestId(elementId.emailError).should('not.exist')
+
+    cy.getByTestId(elementId.pwInput)
+      .should('have.attr', 'type', 'password')
+      .and('be.enabled')
+      .and('be.empty')
+      .and('have.attr', 'placeholder', 'Password')
+    cy.getByTestId(elementId.pwError).should('not.exist')
+
+    cy.getByTestId(elementId.submitButton)
+      .should('be.enabled')
+      .and('have.text', 'Enter')
+
+    cy.getByTestId(elementId.spinner).should('not.exist')
+    cy.getByTestId(elementId.formError).should('not.exist')
   })
 })
