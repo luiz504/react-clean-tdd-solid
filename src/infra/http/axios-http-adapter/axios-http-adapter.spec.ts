49372470f56f4394138cfed014dca32ec414374d
luiz bueno <luiz504@hotmail.com>
2024-08-11 18:47:54 -0300

    refactor:  AxiosHttpAdapter test group

--- src/infra/http/axios-http-adapter/axios-http-adapter.spec.ts
+++ src/infra/http/axios-http-adapter/axios-http-adapter.spec.ts
@@ -21,60 +21,61 @@ describe('AxiosHttpAdapter', () => {
   beforeEach(() => {
     vi.clearAllMocks()
   })
-  it('Should call axios with correct values', () => {
-    const { sut, mockedAxios } = makeSut()
+  describe('post', () => {
+    it('Should call axios.post with correct values', () => {
+      const { sut, mockedAxios } = makeSut()
 
-    const request = mockPostRequest()
-    sut.post(request)
-    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
-  })
-
-  it('Should return the correct statusCode and body', async () => {
-    const mockedAxiosResult = {
-      status: faker.number.int(),
-      data: faker.finance.currency(),
-    }
-    const { sut, mockedAxios } = makeSut()
-    mockedAxios.post.mockResolvedValue(mockedAxiosResult)
-
-    const httpResponse = await sut.post(mockPostRequest())
-
-    expect(httpResponse).toEqual({
-      statusCode: mockedAxiosResult.status,
-      body: mockedAxiosResult.data,
+      const request = mockPostRequest()
+      sut.post(request)
+      expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
     })
-  })
-  it('should return the correct statusCode and body on Failure without response', async () => {
-    const { sut, mockedAxios } = makeSut()
+    it('Should return correct response on axios.post', async () => {
+      const mockedAxiosResult = {
+        status: faker.number.int(),
+        data: faker.finance.currency(),
+      }
+      const { sut, mockedAxios } = makeSut()
+      mockedAxios.post.mockResolvedValue(mockedAxiosResult)
 
-    mockedAxios.post.mockRejectedValueOnce(new Error())
+      const httpResponse = await sut.post(mockPostRequest())
 
-    const promise = await sut.post(mockPostRequest())
-
-    expect(promise).toEqual({
-      statusCode: HttpStatusCode.serverError,
+      expect(httpResponse).toEqual({
+        statusCode: mockedAxiosResult.status,
+        body: mockedAxiosResult.data,
+      })
     })
-  })
-  it('should return the correct statusCode and body on Failure with response', async () => {
-    const { sut, mockedAxios } = makeSut()
+    it('should return correct response on Failure without response on axios.post', async () => {
+      const { sut, mockedAxios } = makeSut()
 
-    const mockedErrorResponse = mockHttpResponse()
+      mockedAxios.post.mockRejectedValueOnce(new Error())
 
-    // Create AxiosError
-    const axiosError = new AxiosError('any_message', 'any_code')
+      const promise = await sut.post(mockPostRequest())
 
-    // Manually set the response property
-    axiosError.response = {
-      status: mockedErrorResponse.status,
-      data: mockedErrorResponse.data,
-    } as any
+      expect(promise).toEqual({
+        statusCode: HttpStatusCode.serverError,
+      })
+    })
+    it('should return correct response on Failure with response on axios.post', async () => {
+      const { sut, mockedAxios } = makeSut()
 
-    mockedAxios.post.mockRejectedValueOnce(axiosError)
+      const mockedErrorResponse = mockHttpResponse()
 
-    const promise = await sut.post(mockPostRequest())
-    expect(promise).toEqual({
-      statusCode: mockedErrorResponse.status,
-      body: mockedErrorResponse.data,
+      // Create AxiosError
+      const axiosError = new AxiosError('any_message', 'any_code')
+
+      // Manually set the response property
+      axiosError.response = {
+        status: mockedErrorResponse.status,
+        data: mockedErrorResponse.data,
+      } as any
+
+      mockedAxios.post.mockRejectedValueOnce(axiosError)
+
+      const promise = await sut.post(mockPostRequest())
+      expect(promise).toEqual({
+        statusCode: mockedErrorResponse.status,
+        body: mockedErrorResponse.data,
+      })
     })
   })
 })
