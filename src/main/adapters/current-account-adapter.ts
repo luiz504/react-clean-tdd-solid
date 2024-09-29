a0511b78bb9d9fc9b675b2dff265410311a01ff7
luiz bueno <luiz504@hotmail.com>
2024-09-29 11:27:18 -0300

    feat: implement getCurrentAccountAdapter

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -1,11 +1,35 @@
-import { UnexpectedError } from '~/domain/errors'
+import { ZodError } from 'zod'
+import {
+  InvalidResourceFormatError,
+  ResourceNotFoundError,
+  UnexpectedError,
+} from '~/domain/errors'
 import { AccountModel, accountModelSchema } from '~/domain/models'
 import { makeLocalStorageAdapter } from '../factories/cache/local-storage-adapter-factory'
 
 export const setCurrentAccountAdapter = (account: AccountModel): void => {
   const safeParse = accountModelSchema.safeParse(account)
   if (!safeParse.success) {
-    throw new UnexpectedError()
+    throw new InvalidResourceFormatError()
   }
   makeLocalStorageAdapter().set('account', account)
+}
+
+export const getCurrentAccountAdapter = (): AccountModel => {
+  const accountStored = makeLocalStorageAdapter().get('account')
+  if (!accountStored) {
+    throw new ResourceNotFoundError()
+  }
+  try {
+    const parsed = JSON.parse(accountStored)
+    const account = accountModelSchema.parse(parsed)
+
+    return account
+  } catch (err) {
+    if (err instanceof ZodError) {
+      throw new InvalidResourceFormatError()
+    }
+
+    throw new UnexpectedError()
+  }
 }
