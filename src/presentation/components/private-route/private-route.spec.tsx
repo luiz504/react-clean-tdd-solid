2dc0afb19040e7e77fa777f2e35765533936a439
luiz bueno <luiz504@hotmail.com>
2024-09-29 15:17:03 -0300

    test: ensure PrivateRoute component if accessToken is not empty

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -1,27 +1,42 @@
 import { render, screen, waitFor } from '@testing-library/react'
 import { PrivateRoute } from '.'
 import { MemoryRouter, Route, Routes } from 'react-router-dom'
+import { ApiContext } from '~/presentation/context/api-context/context'
 
-const makeSut = () => {
+import { mockAccountModel } from '~/domain/__test__'
+import { AccountModel } from '~/domain/models'
+
+const makeSut = (account: AccountModel | null = mockAccountModel()) => {
   return (
-    <MemoryRouter initialEntries={['/']}>
-      <Routes>
-        <Route
-          path="/"
-          element={<PrivateRoute element={<div data-testid="home" />} />}
-        />
-        <Route path="/login" element={<div data-testid="login" />} />
-      </Routes>
-    </MemoryRouter>
+    <ApiContext.Provider
+      value={{ getCurrentAccount: () => account, setCurrentAccount: vi.fn() }}
+    >
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route
+            path="/"
+            element={<PrivateRoute element={<div data-testid="home" />} />}
+          />
+          <Route path="/login" element={<div data-testid="login" />} />
+        </Routes>
+      </MemoryRouter>
+    </ApiContext.Provider>
   )
 }
+
 describe('PrivateRoute', () => {
   it('should redirect to /login if token is empty', async () => {
-    render(makeSut())
+    render(makeSut(null))
 
     await waitFor(() => {
       expect(screen.getByTestId('login')).toBeInTheDocument()
     })
     expect(screen.queryByTestId('home')).not.toBeInTheDocument()
+  })
+  it('should not redirect to /login if token is not empty', async () => {
+    render(makeSut())
+    await waitFor(() => {
+      expect(screen.getByTestId('home')).toBeInTheDocument()
+    })
   })
 })
