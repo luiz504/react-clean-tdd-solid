a7aa896ccf2f6f582a9e937b48d32e7c900b27b2
luiz bueno <luiz504@hotmail.com>
2024-09-29 15:16:11 -0300

    feat: ensure PrivateRoute component if accessToken is not empty

--- src/presentation/components/private-route/index.tsx
+++ src/presentation/components/private-route/index.tsx
@@ -1,9 +1,12 @@
 import { FC } from 'react'
 import { Navigate } from 'react-router-dom'
+import { useApiContext } from '~/presentation/context/api-context/hook'
 
 type Props = {
   element: JSX.Element
 }
-export const PrivateRoute: FC<Props> = () => {
-  return <Navigate to="/login" />
+export const PrivateRoute: FC<Props> = ({ element }) => {
+  const { getCurrentAccount } = useApiContext()
+  const accessToken = getCurrentAccount()?.accessToken
+  return accessToken ? element : <Navigate to="/login" />
 }
