de7ddf438539c3ef190518a00a88ddcc5c2e37cf
luiz bueno <luiz504@hotmail.com>
2024-10-05 20:23:30 -0300

    test: ensure AuthorizeHttpGetClientDecorator add header if there is an acessToken

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
@@ -6,6 +6,7 @@ import {
 import { AuthorizeHttpGetClientDecorator } from '.'
 import { faker } from '@faker-js/faker'
 import { HttpGetParams } from '~/data/protocols/http'
+import { mockAccountModel } from '~/domain/__test__'
 
 const makeSut = () => {
   const getStorageSpy = new GetStorageSpy()
@@ -27,8 +28,10 @@ describe('AuthorizeHttpGetClientDecorator', () => {
     await sut.get(mockGetRequest())
     expect(getStorageSpy.key).toBe('account')
   })
-  it('should not add header if GetStorage returns null', async () => {
-    const { sut, httpGetClientSpy } = makeSut()
+  it.only('should not add header if GetStorage returns null', async () => {
+    const { sut, getStorageSpy, httpGetClientSpy } = makeSut()
+    getStorageSpy.value = null
+
     const httpRequest: HttpGetParams = {
       url: faker.internet.url(),
       headers: {
@@ -38,5 +41,19 @@ describe('AuthorizeHttpGetClientDecorator', () => {
     await sut.get(httpRequest)
     expect(httpGetClientSpy.url).toBe(httpRequest.url)
     expect(httpGetClientSpy.headers).toEqual(httpRequest.headers)
+  })
+  it('should add header to HttpGetClient header if GetStorage is not null', async () => {
+    const { sut, getStorageSpy, httpGetClientSpy } = makeSut()
+    const account = mockAccountModel()
+
+    getStorageSpy.value = JSON.stringify(account)
+    const httpRequest: HttpGetParams = {
+      url: faker.internet.url(),
+    }
+    await sut.get(httpRequest)
+    expect(httpGetClientSpy.url).toEqual(httpRequest.url)
+    expect(httpGetClientSpy.headers).toEqual({
+      'x-access-token': account.accessToken,
+    })
   })
 })
