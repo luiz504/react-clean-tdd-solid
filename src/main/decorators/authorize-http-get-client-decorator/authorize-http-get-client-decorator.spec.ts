1399e9aac7b725fad589c38091ff9b5506ebd899
luiz bueno <luiz504@hotmail.com>
2024-10-05 19:52:47 -0300

    refactor: move SUT to its own factory

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
@@ -1,10 +1,18 @@
 import { GetStorageSpy } from '~/data/__test__'
 import { AuthorizeHttpGetClientDecorator } from '.'
 
+const makeSut = () => {
+  const getStorageSpy = new GetStorageSpy()
+  const sut = new AuthorizeHttpGetClientDecorator(getStorageSpy)
+  return {
+    sut,
+    getStorageSpy,
+  }
+}
+
 describe('AuthorizeHttpGetClientDecorator', () => {
   it('should call GetStorage with correct value', async () => {
-    const getStorageSpy = new GetStorageSpy()
-    const sut = new AuthorizeHttpGetClientDecorator(getStorageSpy)
+    const { sut, getStorageSpy } = makeSut()
     sut.get()
     expect(getStorageSpy.key).toBe('account')
   })
