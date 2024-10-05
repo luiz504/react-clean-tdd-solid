6cd3b40c1501ae2f0adc5cb1887fff4562537e5a
luiz bueno <luiz504@hotmail.com>
2024-10-05 19:23:46 -0300

    feat: ensure AxiosHttpClient calls axios.get with correct values

--- src/data/protocols/http/http-get-client.ts
+++ src/data/protocols/http/http-get-client.ts
@@ -2,6 +2,7 @@ import { HttpResponse } from './http-response'
 
 export type HttpGetParams = {
   url: string
+  headers?: Record<string, string>
 }
 export interface HttpGetClient {
   get: <R>(params: HttpGetParams) => Promise<HttpResponse<R>>
