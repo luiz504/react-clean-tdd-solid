e390bcb9a71611ae29adb4d1c61deca19025947a
luiz bueno <luiz504@hotmail.com>
2024-10-06 16:07:21 -0300

    chore: add react-query devtools

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -6,10 +6,12 @@ import '~/presentation/styles/global.css'
 import { Router } from './router'
 
 import { queryClient } from '~/infra/cache/query-client'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <Router />
     </QueryClientProvider>
   </React.StrictMode>,
