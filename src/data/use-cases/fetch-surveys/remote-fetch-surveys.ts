fa8f30bf4c94ac47049f42254a72002e6915227b
luiz bueno <luiz504@hotmail.com>
2024-10-06 13:34:00 -0300

    feat: ensure RemoteFetchSurveys returs FetchSurveysModel

--- src/data/use-cases/fetch-surveys/remote-fetch-surveys.ts
+++ src/data/use-cases/fetch-surveys/remote-fetch-surveys.ts
@@ -1,12 +1,19 @@
 import { HttpGetClient, HttpStatusCode } from '~/data/protocols/http'
 import { UnexpectedError } from '~/domain/errors'
+import { SurveyAnswerModel } from '~/domain/models'
 
 import {
   FetchSurveys,
   FetchSurveysModel,
 } from '~/domain/use-cases/fetch-surveys'
 
-type RemoteFetchSurveysModel = FetchSurveysModel
+export type RemoteFetchSurveysModel = {
+  id: string
+  question: string
+  answers: SurveyAnswerModel[]
+  date: string
+  didAnswer: boolean
+}
 export class RemoteFetchSurveys implements FetchSurveys {
   constructor(
     private readonly url: string,
@@ -14,13 +21,18 @@ export class RemoteFetchSurveys implements FetchSurveys {
   ) {}
 
   async fetch(): Promise<FetchSurveysModel> {
-    const httpResponse = await this.httpGetClient.get<RemoteFetchSurveysModel>({
+    const httpResponse = await this.httpGetClient.get<
+      RemoteFetchSurveysModel[]
+    >({
       url: this.url,
     })
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
         if (Array.isArray(httpResponse.body)) {
-          return httpResponse.body
+          return httpResponse.body.map((item) => ({
+            ...item,
+            date: new Date(item.date),
+          }))
         }
         throw new UnexpectedError()
       case HttpStatusCode.noContent:
