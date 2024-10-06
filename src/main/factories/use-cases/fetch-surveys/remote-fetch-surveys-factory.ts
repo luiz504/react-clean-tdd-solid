b3314f7a6fdd6a76c2346b478b6f51d9ef22738a
luiz bueno <luiz504@hotmail.com>
2024-10-06 13:12:44 -0300

    feat: add AuthorizeHttpGetClientDecorator factory

--- src/main/factories/use-cases/fetch-surveys/remote-fetch-surveys-factory.ts
+++ src/main/factories/use-cases/fetch-surveys/remote-fetch-surveys-factory.ts
@@ -1,10 +1,10 @@
 import { RemoteFetchSurveys } from '~/data/use-cases/fetch-surveys/remote-fetch-surveys'
-import { makeAxiosHttpAdapter } from '../../http/axios-http-adapter-factory'
 import { makeApiUrl } from '../../http/api-url-factory'
+import { makeAuthorizeHttpGetClientDecorator } from '../decorators'
 
 export const makeRemoteFetchSurveys = (): RemoteFetchSurveys => {
   return new RemoteFetchSurveys(
     makeApiUrl('api/surveys').toString(),
-    makeAxiosHttpAdapter(),
+    makeAuthorizeHttpGetClientDecorator(),
   )
 }
