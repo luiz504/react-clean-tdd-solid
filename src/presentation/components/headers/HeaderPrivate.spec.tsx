a0a254e512b160c037d8218857e3a062274f8163
luiz bueno <luiz504@hotmail.com>
2024-10-06 14:34:06 -0300

    test: ensure HeaderPrivate shows user name

--- src/presentation/components/headers/HeaderPrivate.spec.tsx
+++ src/presentation/components/headers/HeaderPrivate.spec.tsx
@@ -3,8 +3,10 @@ import { HeaderPrivate } from './HeaderPrivate'
 
 import { ApiContext } from '~/presentation/context/api-context/context'
 import { MemoryRouter } from 'react-router-dom'
+import { mockAccountModel } from '~/domain/__test__'
 const ELEMENTS_TEST_ID = {
   'logout-btn': 'logout-btn',
+  'user-name': 'user-name',
 } as const
 const mockedNavigate = vi.fn()
 vi.mock('react-router-dom', async () => {
@@ -15,13 +17,13 @@ vi.mock('react-router-dom', async () => {
   }
 })
 
-const makeSut = () => {
+const makeSut = (account = mockAccountModel()) => {
   const setCurrentAccountMock = vi.fn()
   render(
     <MemoryRouter>
       <ApiContext.Provider
         value={{
-          getCurrentAccount: vi.fn(),
+          getCurrentAccount: () => account,
           setCurrentAccount: setCurrentAccountMock,
         }}
       >
@@ -40,5 +42,15 @@ describe('Component: HeaderPrivate', () => {
     fireEvent.click(screen.getByTestId(ELEMENTS_TEST_ID['logout-btn']))
     expect(setCurrentAccountMock).toHaveBeenCalledWith(null)
     expect(mockedNavigate).toHaveBeenCalledWith('/sign-in')
+  })
+
+  it('should render userName correctly', () => {
+    const account = mockAccountModel()
+    const { setCurrentAccountMock } = makeSut(account)
+    const userName = 'any_user'
+    setCurrentAccountMock(userName)
+    expect(screen.getByTestId(ELEMENTS_TEST_ID['user-name'])).toHaveTextContent(
+      account.name,
+    )
   })
 })
